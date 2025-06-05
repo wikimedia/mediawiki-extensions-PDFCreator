87f93edcd11cfa666aafb01a7d9cf7e15f68645f
hslater <hamish.slater@hallowelt.com>
2025-06-05 21:40:35 +1200

    Replace deprecated MWException
    ERM38444
    
    [REL1_43, REL1_43-1.1.x, master]

--- src/MediaWiki/Content/PDFCreatorTemplate.php
+++ src/MediaWiki/Content/PDFCreatorTemplate.php
@@ -2,15 +2,15 @@
 
 namespace MediaWiki\Extension\PDFCreator\MediaWiki\Content;
 
+use InvalidArgumentException;
 use MediaWiki\Content\TextContent;
-use MWException;
 
 class PDFCreatorTemplate extends TextContent {
 
 	/**
 	 * @param string $text
 	 *
-	 * @throws MWException
+	 * @throws InvalidArgumentException
 	 */
 	public function __construct( $text ) {
 		parent::__construct( $text, 'pdfcreator_template' );
