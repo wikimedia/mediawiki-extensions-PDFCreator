13c920c8eb78fb5d9f2d533369ce46256252913c
Mschlindwein <schlindwein@hallowelt.com>
2025-10-31 10:40:14 +0100

    Add pdf template overview in global actions
    ERM44869
    
    [5.x]
    
    Change-Id: Ifd00172775e704e300226589560e4855b8e2158d

--- src/MediaWiki/Hook/BlueSpiceDiscoverySkinHandler.php
+++ src/MediaWiki/Hook/BlueSpiceDiscoverySkinHandler.php
@@ -3,8 +3,11 @@
 namespace MediaWiki\Extension\PDFCreator\MediaWiki\Hook;
 
 use MediaWiki\Extension\PDFCreator\Component\ExportDialogButton;
+use MediaWiki\Message\Message;
 use MediaWiki\Permissions\PermissionManager;
+use MediaWiki\SpecialPage\SpecialPageFactory;
 use MediaWiki\Title\NamespaceInfo;
+use MWStake\MediaWiki\Component\CommonUserInterface\Component\RestrictedTextLink;
 use MWStake\MediaWiki\Component\CommonUserInterface\Hook\MWStakeCommonUIRegisterSkinSlotComponents;
 
 class BlueSpiceDiscoverySkinHandler implements MWStakeCommonUIRegisterSkinSlotComponents {
@@ -15,13 +18,19 @@ class BlueSpiceDiscoverySkinHandler implements MWStakeCommonUIRegisterSkinSlotCo
 	/** @var PermissionManager */
 	private $permissionManager;
 
+	/** @var SpecialPageFactory */
+	private $specialPageFactory;
+
 	/**
 	 * @param NamespaceInfo $namespaceInfo
 	 * @param PermissionManager $permissionManager
+	 * @param SpecialPageFactory $specialPageFactory
 	 */
-	public function __construct( NamespaceInfo $namespaceInfo, PermissionManager $permissionManager ) {
+	public function __construct( NamespaceInfo $namespaceInfo, PermissionManager $permissionManager,
+		SpecialPageFactory $specialPageFactory ) {
 		$this->namespaceInfo = $namespaceInfo;
 		$this->permissionManager = $permissionManager;
+		$this->specialPageFactory = $specialPageFactory;
 	}
 
 	/**
@@ -40,5 +49,22 @@ class BlueSpiceDiscoverySkinHandler implements MWStakeCommonUIRegisterSkinSlotCo
 				]
 			]
 		);
+
+		$specialOverview = $this->specialPageFactory->getTitleForAlias( 'PDFTemplatesOverview' );
+		$overviewEntry = [
+			'bs-special-pdf-templates' => [
+				'factory' => static function () use ( $specialOverview ) {
+					return new RestrictedTextLink( [
+						'id' => 'ga-bs-pdf-templates',
+						'href' => $specialOverview->getLocalURL(),
+						'text' => Message::newFromKey( 'pdfcreator-global-action-overview' ),
+						'title' => Message::newFromKey( 'pdfcreator-global-action-overview-desc' ),
+						'aria-label' => Message::newFromKey( 'pdfcreator-global-action-overview' ),
+						'permissions' => [ 'read' ]
+					] );
+				}
+			]
+		];
+		$registry->register( 'GlobalActionsOverview', $overviewEntry );
 	}
 }
