f64f5c18a9a2090b4e41df2d2ba0389e1bd6d7f4
Daniel Vogel <daniel.vogel@hallowelt.com>
2026-02-05 08:15:01 +0100

    Export pdf with first available pdf template if requested template is deleted
    [5.1.x]
    
    ERM46220
    
    Change-Id: I78a71959c7af1530cb02c293135ef202b01d39aa
    (cherry picked from commit 2e683fd9ab2d20d085cad866b5879d1c0c893fe9)

--- src/TemplateProvider/Wiki.php
+++ src/TemplateProvider/Wiki.php
@@ -70,6 +70,14 @@ class Wiki implements ITemplateProvider {
 	 * @return Template|null
 	 */
 	public function getTemplate( ExportContext $context, string $name = '' ): ?Template {
+		$allTemplateNames = $this->getTemplateNames();
+		if ( count( $allTemplateNames ) === 0 ) {
+			return null;
+		}
+		if ( !in_array( $name, $allTemplateNames ) ) {
+			$name = $allTemplateNames[0];
+		}
+
 		$templateTitle = $this->util->createPDFTemplateTitle( $name );
 		$revId = $templateTitle->getLatestRevID();
 
