8c4e8f381cf3f681fdb9bf0eab8cd3846ea81f3b
Robert Vogel <vogel@hallowelt.biz>
2026-04-27 11:55:59 +0200

    Add support for `thumb.php`
    ERM47579
    [ai authored]
    [5.x]
    
    Change-Id: Ib313f1cb28f6a9426eb15e1400497e24ef2dfd4e

--- tests/phpunit/FileResolverTest.php
+++ tests/phpunit/FileResolverTest.php
@@ -6,6 +6,7 @@ use BlueSpice\CloudClient\PDFTemplatePlaceholderParams\Title;
 use DOMDocument;
 use DOMElement;
 use File;
+use MediaWiki\Config\Config;
 use MediaWiki\Extension\PDFCreator\Utility\FileResolver;
 use MediaWiki\MainConfigNames;
 use MediaWikiLangTestCase;
@@ -13,6 +14,7 @@ use RepoGroup;
 
 /**
  * @covers \MediaWiki\Extension\PDFCreator\Utility\FileResolver::execute
+ * @covers \MediaWiki\Extension\PDFCreator\Utility\FileResolver::resolveFromThumbScript
  */
 class FileResolverTest extends MediaWikiLangTestCase {
 
@@ -44,6 +46,72 @@ class FileResolverTest extends MediaWikiLangTestCase {
 
 			$this->assertEquals( 'Example.jpg', $file->getName() );
 		}
+	}
+
+	/**
+	 * @covers \MediaWiki\Extension\PDFCreator\Utility\FileResolver::resolveFromThumbScript
+	 * @dataProvider provideThumbScriptSrcs
+	 *
+	 * @param string $src
+	 * @param string|null $expectedName null means the file should not be resolved
+	 */
+	public function testResolveFromThumbScript( string $src, ?string $expectedName ): void {
+		$config = $this->createMock( Config::class );
+		$config->method( 'get' )->willReturnCallback( static function ( string $key ): string {
+			return match ( $key ) {
+				'ThumbnailScriptPath' => '/w/thumb.php',
+				'Server'              => 'https://example.com',
+				'UploadPath'          => '/images',
+				'ScriptPath'          => '/w',
+				default               => '',
+			};
+		} );
+
+		$repoGroup = $this->mockRepoGroup();
+		$titleFactory = $this->getServiceContainer()->getTitleFactory();
+
+		$dom = new DOMDocument();
+		$dom->loadXML( '<html><body><img src="' . htmlspecialchars( $src ) . '"/></body></html>' );
+		$img = $dom->getElementsByTagName( 'img' )->item( 0 );
+		$this->assertInstanceOf( DOMElement::class, $img );
+
+		$fileResolver = new FileResolver( $config, $repoGroup, $titleFactory );
+		$file = $fileResolver->execute( $img );
+
+		if ( $expectedName !== null ) {
+			$this->assertNotNull( $file, "Expected file to be resolved from: $src" );
+			$this->assertEquals( $expectedName, $file->getName() );
+		} else {
+			$this->assertNull( $file, "Expected no file to be resolved from: $src" );
+		}
+	}
+
+	/**
+	 * @return array[]
+	 */
+	public function provideThumbScriptSrcs(): array {
+		return [
+			'relative thumb.php URL resolves file' => [
+				'/w/thumb.php?f=Example.jpg&width=300',
+				'Example.jpg',
+			],
+			'absolute thumb.php URL resolves file' => [
+				'https://example.com/w/thumb.php?f=Example.jpg&width=120',
+				'Example.jpg',
+			],
+			'thumb.php URL with unknown file returns null' => [
+				'/w/thumb.php?f=NoSuchFile.jpg&width=300',
+				null,
+			],
+			'thumb.php URL without f param returns null' => [
+				'/w/thumb.php?width=300',
+				null,
+			],
+			'non-matching script path is not handled by resolveFromThumbScript' => [
+				'/w/other.php?f=Example.jpg',
+				null,
+			],
+		];
 	}
 
 	/**
